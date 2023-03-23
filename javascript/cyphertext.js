b838528645ad4d3fc5bdd73ec5a8cbdaf42f77c5
Roboskeletron <wwwrt287@gmail.com>
2023-03-23 22:47:47 +0300

    initial commit, rotors added

---
+++ javascript/cyphertext.js
@@ -0,0 +1,17 @@
+const rotors = [document.getElementById("rotor1"), document.getElementById("rotor2"),
+    document.getElementById("rotor3"), document.getElementById("rotor4")]
+
+function addRotorTypes(rotors) {
+    rotors.forEach(rotor => rotor.insertAdjacentHTML("beforeend",
+        `<option>I</option>
+<option>I</option>
+<option>II</option>
+<option>III</option>
+<option>IV</option>
+<option>V</option>
+<option>VI</option>
+<option>VII</option>
+<option>VIII</option>`))
+}
+
+addRotorTypes(rotors)
